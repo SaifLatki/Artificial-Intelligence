8937150f30a5d90653591acaab743d81453f0928
Saif <saifullahlatki338@gmail.com>
2025-01-28 11:47:30 +0500

    use of input in python

---
+++ All Labs/inputTasks/task03.py
@@ -0,0 +1,12 @@
+num1=input("Enter a number: ")
+num2=input("Enter another number: ")
+print("Befre swapping")
+print ("The first number is: ",num1)
+print ("The second number is: ",num2)
+temp=0
+temp=num1
+num1=num2
+num2=temp
+print("After swapping")
+print ("The first number is: ",num1)
+print ("The second number is: ",num2)
---
+++ All Labs/lab01/mathFunc.py
@@ -0,0 +1,13 @@
+import math
+
+x=math.sqrt(25)
+print(x)
+print(math.floor(3.522))
+print(math.ceil(7.20130))
+
+
+x=input("Enter a number: ")
+num1=int(x)
+y=input("Enter another number: ")
+num2=int(y)
+print("The sum of the two numbers is: ",num1+num2)
